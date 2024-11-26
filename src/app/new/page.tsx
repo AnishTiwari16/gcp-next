584e0197d00f75084e9b0b897c1ada2951cbaffc
Anish Tiwari <87610940+AnishTiwari16@users.noreply.github.com>
2024-11-27 00:11:51 +0530

    Feat

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Page = () => {
-    return <div>page</div>;
+    return (
+        <div>
+            page <div>{process.env.NEXT_PUBLIC_TITLE}</div>
+        </div>
+    );
 };
 
 export default Page;
